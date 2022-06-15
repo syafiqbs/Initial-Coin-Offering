72e532ee7d5a10da63853dea291929b70ffe0797
syafiqbs <cross.symbol@hotmail.com>
2022-06-15 22:32:04 +0800

    Moved project files into repo

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x35913a47e481D3207568FBa2B0F9816375332d3D";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
